c2800e48a5d165a46d675d087183a04573c5c867
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-19 21:04:54 +0530

    Completed

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import Words from "./Words";
-
 const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
@@ -9,3 +7,115 @@ const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
+
+const words = [
+  'sigh',
+  'tense',
+  'airplane',
+  'ball',
+  'pies',
+  'juice',
+  'warlike',
+  'bad',
+  'north',
+  'dependent',
+  'steer',
+  'silver',
+  'highfalutin',
+  'superficial',
+  'quince',
+  'eight',
+  'feeble',
+  'admit',
+  'drag',
+  'loving'
+];
+
+//Init Word
+let randomWord;
+
+// Init Score
+
+let score = 0;
+
+// Init Time
+let time = 15;
+// Set difficulty to default medium or else grab from localStorage
+let difficulty = localStorage.getItem("difficulty") !== null ? localStorage.getItem("difficulty") : "medium";
+difficultySelect.value = difficulty
+
+// Generates Random word
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length) - 1]
+
+}
+
+// Add Word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord()
+  word.innerHTML = randomWord
+}
+function updateScore(word) {
+  score = score + word.length
+  scoreEl.innerHTML = score
+}
+function decreaseScore(randomWord) {
+  if (score > randomWord.length) {
+    score = score - randomWord.length
+    scoreEl.innerHTML = score;
+  }
+}
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + " s"
+  if (time === 0) {
+    clearInterval(timeInterval)
+
+    gameOver()
+  }
+}
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>You Ran Out Of Time Babai</h1>
+    <p>Your Final Score is ${score}</p>
+    <button onClick="location.reload()">Play Again</button>
+  `
+  endgameEl.style.display = "flex"
+}
+addWordToDOM()
+
+text.focus()
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+//Text Listener
+text.addEventListener("input", e => {
+  const insertedText = e.target.value;
+  if (insertedText === randomWord) {
+    addWordToDOM()
+    updateScore(insertedText)
+    e.target.value = ""
+    if (difficulty === "hard") {
+      time += 3;
+    } else if (difficulty === "medium") {
+      time += 4
+    } else {
+      time += 6
+    }
+    updateTime()
+  }
+  if (insertedText.length === randomWord.length && insertedText !== randomWord) {
+    decreaseScore(randomWord)
+  }
+})
+// settings btn onClick
+
+settingsBtn.addEventListener("click", () => {
+  settings.classList.toggle("hide")
+})
+// Settings Select
+
+settingsForm.addEventListener("change", e => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty)
+})
--- words.js
+++
@@ -1,4 +0,0 @@
-const Words = require('an-array-of-english-words')
-
-export default Words;
-
